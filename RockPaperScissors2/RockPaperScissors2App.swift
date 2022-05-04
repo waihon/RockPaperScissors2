1424eea4b87e6676eb0b035c5c991c4904be475e
Waihon Yew <yewwaihon@gmail.com>
2022-05-04 15:35:20 +0800

    Initial Commit

---
+++ RockPaperScissors2/RockPaperScissors2App.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissors2App.swift
+//  RockPaperScissors2
+//
+//  Created by Waihon Yew on 04/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissors2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
